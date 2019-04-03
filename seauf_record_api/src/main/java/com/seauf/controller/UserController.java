271c305529fbf91acb7eac0947373c05c571d171
fenghaiyang <fenghaiyang@jiangduoduo.com>
2019-04-03 19:21:55 +0800

    初始化

---
+++ seauf_record_api/src/main/java/com/seauf/controller/UserController.java
@@ -0,0 +1,67 @@
+package com.seauf.controller;
+
+import com.alibaba.fastjson.JSON;
+import entity.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @description:
+ * @author: lynn
+ * @create: 2019/02/21
+ */
+@Controller
+@RequestMapping(value = {"user", ""})
+public class UserController {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    @PostMapping("register ")
+    public User register (HttpServletRequest req) {
+        User user = new User();
+        user.setName("lynn");
+        List<User> users = new ArrayList<>();
+        users.add(user);
+        stringRedisTemplate.opsForValue().set("users", JSON.toJSONString(users));
+        return user;
+    }
+
+    @PostMapping("info")
+    public User userInfo(HttpServletRequest req) {
+        User user = new User();
+        user.setName("lynn");
+        List<User> users = new ArrayList<>();
+        users.add(user);
+        stringRedisTemplate.opsForValue().set("users", JSON.toJSONString(users));
+        return user;
+    }
+
+    @RequestMapping()
+    public String index() {
+        return "login";
+    }
+
+    @RequestMapping("test")
+    public String userInfo() {
+        return "test";
+    }
+
+    @RequestMapping("game")
+    public String game() {
+        return "game";
+    }
+
+
+}
---
+++ seauf_record_api/src/main/java/com/seauf/controller/vo/UserVO.java
@@ -0,0 +1,39 @@
+package com.seauf.controller.vo;
+
+/**
+ * @description:
+ * @author: lynn
+ * @create: 2019/04/03
+ */
+public class UserVO {
+
+    private String phone;
+
+    private String email;
+
+    private String password;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
