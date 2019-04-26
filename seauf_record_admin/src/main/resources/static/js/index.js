b111b7e157a1ecfd3f23e4af48cfa3d654a64eac
fenghaiyang <fenghaiyang@jiangduoduo.com>
2019-04-26 18:57:32 +0800

    初始化

--- seauf_record_admin/src/main/resources/static/js/index.js
+++ seauf_record_admin/src/main/resources/static/js/index.js
@@ -1,9 +1,3 @@
 function selectPage(pageName) {
-    if (pageName == "1") {
-        $(".dashboard2").css("display", "none");
-        $(".dashboard1").css("display", "block");
-    } else {
-        $(".dashboard1").css("display", "none");
-        $(".dashboard2").css("display", "block");
-    }
+    window.location = pageName;
 }
