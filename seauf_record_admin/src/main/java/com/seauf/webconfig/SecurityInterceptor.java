ab3c400cf6a6d2b40923d94a6f853a168dc58014
fenghaiyang <fenghaiyang@jiangduoduo.com>
2019-07-17 14:04:02 +0800

    sso

---
+++ seauf_record_admin/src/main/java/com/seauf/webconfig/SecurityInterceptor.java
@@ -0,0 +1,31 @@
+package com.seauf.webconfig;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+/**
+ * @description:
+ * @author: lynn
+ * @create: 2019/07/16
+ */
+public class SecurityInterceptor extends HandlerInterceptorAdapter {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws IOException {
+        HttpSession session = request.getSession();
+
+//            判断是否已有该用户登录的session
+        if (false) {
+            return true;
+        }
+
+//            跳转到登录页
+        String url = "/login";
+        response.sendRedirect(url);
+        return false;
+    }
+}
--- seauf_record_service/src/main/java/com/seauf/controller/IndexController.java
+++ seauf_record_service/src/main/java/com/seauf/controller/IndexController.java
@@ -15,4 +15,9 @@ public class IndexController {
     public String index() {
         return "index";
     }
+
+    @RequestMapping("login")
+    public String login() {
+        return "login";
+    }
 }
